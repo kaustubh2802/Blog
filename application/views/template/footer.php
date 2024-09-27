f9ab5905444dde7f166d81ae46ed7b1b97468b48
kaustubh2802 <kbnitro5@gmail.com>
2024-09-27 13:16:35 +0530

    bootstrap datatables

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,44 +1,61 @@
 <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
 <!-- <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script> -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 
 
 
-    <!-- DataTables JS -->
-    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
-    <!-- <script src="cdn.datatables.net/2.1.7/js/dataTables.min.js"></script> -->
+<!-- DataTables JS -->
+<!-- <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script> -->
+<!-- <script src="cdn.datatables.net/2.1.7/js/dataTables.min.js"></script> -->
 
-    <script>
-      // let table = new DataTable('#myTableOne');
-      $(document).ready(function() {
+
+<!-- for bootstrap -->
+<script src="  https://code.jquery.com/jquery-3.7.1.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js "></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/js/bootstrap.min.js "></script>
+<script src=" https://cdn.datatables.net/2.1.7/js/dataTables.js"></script>
+<script src="https://cdn.datatables.net/2.1.7/js/dataTables.bootstrap4.js "></script>
+
+
+
+
+
+
+
+
+
+
+
+
+<script>
+  // let table = new DataTable('#myTableOne');
+  $(document).ready(function() {
     $('#myTableOne').DataTable(); // Initialize DataTables
   });
+</script>
 
-
-    </script>
-
-    <!-- confirm-delete -->
-    <script> 
-      $(document).ready(function () {
-        $('.confirm-delete').click(function(e){
-          e.preventDefault(); 
-          confirmDialog = confirm("Are you sure? you want to delete this data?");          
-          if(confirmDialog)
-          {
-            var id = $(this).val();
-            // alert(id);
-            $.ajax({
-              type: "DELETE",
-              url: "/employee/confirmdelete/"+id,
-              success: function (response){
-                alert("Record is deleted !");
-                window.location.reload();
-              }
-            });
-        }
-      });
-      
+<!-- confirm-delete -->
+<script>
+  $(document).ready(function() {
+    $('.confirm-delete').click(function(e) {
+      e.preventDefault();
+      confirmDialog = confirm("Are you sure? you want to delete this data?");
+      if (confirmDialog) {
+        var id = $(this).val();
+        // alert(id);
+        $.ajax({
+          type: "DELETE",
+          url: "/employee/confirmdelete/" + id,
+          success: function(response) {
+            alert("Record is deleted !");
+            window.location.reload();
+          }
+        });
+      }
     });
-    </script> 
-  </body>
+
+  });
+</script>
+</body>
+
 </html>
--- application/views/template/header.php
+++ application/views/template/header.php
@@ -14,7 +14,9 @@
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
 
 
-    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.1.7/css/dataTables.bootstrap4.css">
+ 
     <title>Hello, world!</title>
   </head>
   <body>
