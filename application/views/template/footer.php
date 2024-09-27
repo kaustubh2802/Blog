622134a9a9a4867dd6158eed82731826181c5056
kaustubh2802 <kbnitro5@gmail.com>
2024-09-27 12:32:17 +0530

    working confirm delete

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,28 +1,29 @@
-
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+<!-- <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script> -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 
     <!-- confirm-delete -->
-    <script>
-      $(document).ready(function(){
-
+    <script> 
+      $(document).ready(function () {
         $('.confirm-delete').click(function(e){
-          e.preventDefault();
-          // alert("are you?");
-
-          confirmDialog = confirm("Are you sure? you want to delete this data?");
-          
+          e.preventDefault(); 
+          confirmDialog = confirm("Are you sure? you want to delete this data?");          
           if(confirmDialog)
-        {
+          {
             var id = $(this).val();
             // alert(id);
-            $.ajax
-
+            $.ajax({
+              type: "DELETE",
+              url: "/employee/confirmdelete/"+id,
+              success: function (response){
+                alert("Record is deleted !");
+                window.location.reload();
+              }
+            });
         }
-
       });
       
     });
-    </script>
+    </script> 
   </body>
 </html>
